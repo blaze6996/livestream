6eb2e7319b4e744a031b8792d18836a183361957
blaze6996 <166194492+blaze6996@users.noreply.github.com>
2025-02-01 09:58:21 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    ffmpeg -re -stream_loop -1 -i "https://YOUR_VIDEO_URL_HERE" \
+    ffmpeg -re -stream_loop -1 -i "https://www.youtube.com/live/Eba6OYQgRvA?si=ciI_OHqA5nUujqS0" \
     -vf "scale=1080:1920,format=yuv420p" \
     -c:v libx264 -preset ultrafast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -c:a aac -b:a 128k -ar 44100 \
