0e9b7ee0ce16c146431d3846a0142453770f190e
blaze6996 <166194492+blaze6996@users.noreply.github.com>
2025-02-01 10:30:57 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-
-
-# Replace with the YouTube video URL
+# URL of the YouTube video
 VIDEO_URL="https://www.youtube.com/watch?v=Eba6OYQgRvA"
+COOKIES_FILE="/cookies.txt"  # Path to cookies.txt in the container
 
-# Fetch the stream URL using yt-dlp with username and password
-yt-dlp -u "$YTDLP_USERNAME" -p "$YTDLP_PASSWORD" -f best -o - "$VIDEO_URL" | ffmpeg -re -i pipe:0 \
--vf "scale=1080:1920,format=yuv420p" \
--c:v libx264 -preset ultrafast -b:v 3000k -maxrate 3000k -bufsize 6000k \
--c:a aac -b:a 128k -ar 44100 \
--f flv "rtmp://a.rtmp.youtube.com/live2/m2x0-dkjr-5e1p-vhdf-bahg"
+# Loop to fetch the stream URL and feed it to ffmpeg
+while true; do
+    yt-dlp --cookies "$COOKIES_FILE" -f best -o - "$VIDEO_URL" | \
+    ffmpeg -re -i pipe:0 -vf "scale=1080:1920,format=yuv420p" \
+    -c:v libx264 -preset ultrafast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv "rtmp://a.rtmp.youtube.com/live2/m2x0-dkjr-5e1p-vhdf-bahg"
+    
+    sleep 2
+done
