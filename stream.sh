386a7caf228d3fa633b451612792b6b7fdb4c5a0
blaze6996 <166194492+blaze6996@users.noreply.github.com>
2025-02-01 09:54:59 +0530

    Create stream.sh

---
+++ stream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true; do
+    ffmpeg -re -i "https://YOUR_VIDEO_URL_HERE" \
+    -vf "scale=1080:1920,format=yuv420p" \
+    -c:v libx264 -preset ultrafast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv "rtmp://a.rtmp.youtube.com/live2/YOUR_STREAM_KEY"
+    sleep 2
+done
